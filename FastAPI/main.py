30a90851228eab3b29211b5c371cdf6c4a18ec7b
Madhav <bangarumadhav@gmail.com>
2025-05-25 17:13:49 +0530

    cleaned fatsapi

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -1,5 +1,4 @@
 from fastapi import FastAPI
-from Helpers.sql_helper import MySQLConnector
 app = FastAPI()
 
 @app.get("/")
@@ -11,13 +10,3 @@ def custom():
     with open("commands.txt", "r") as file:
         content = file.read()
     return content
-
-@app.get("/sqldata")
-def sqldata():
-    connector = MySQLConnector(host="127.0.0.1", user="root", password="root", database="people")
-    # Connect to the MySQL server
-    connector.connect()
-
-    # Execute a query
-    query = "select * from sample"
-    return connector.execute_query(query)
